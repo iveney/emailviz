0a4f4ff88edc5413691688c097272220881e48ed
Zigang Xiao <iveney@gmail.com>
2013-01-30 18:11:01 -0600

    Handle exceptions. Ignore no use messages.

--- process_emails.py
+++ process_emails.py
@@ -21,10 +21,16 @@ def main():
 	# iterate all messages and extract information
 	p = re.compile(r'(\b(?:\d{1,3}\.){3}\d{1,3}\b)\]\)')
 	for msg in mbox:
-		ip_str = msg['Received']
-		m = p.search(ip_str)
-		date_str = msg['Date']
-		print m.group(1), date_str
+		try:
+			ip_str = msg['Received']
+			m = p.search(ip_str)
+			date_str = msg['Date']
+			print m.group(1), date_str
+		except:
+			sys.stderr.write("Error: " + str(sys.exc_info()[0]) + "\n")
+			sys.stderr.write("IP string is: " + str(ip_str) + "\n")
+			sys.stderr.write("Message Subject: " + str(msg['subject']) + "\n")
+			continue # ignore this message
 
 if __name__ == '__main__':
 	main()
